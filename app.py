cca323faa2430c89d15b9d8ff15c61b8daa3fd81
marjoryplacide-gif <marjory.placide21@gmail.com>
2026-04-30 16:29:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -596,7 +596,7 @@ if authentication_status:
     elif username == "douane":
         # Vue détaillée pour la douane 
         if 'Reception_douane' in df.columns:
-            df = df[df['Reception_douane'] != "Recu"]
+            df = df[df['Reception_douane'] != "validation douane"]
     st.markdown("### Déclarations à valider par la douane")
     if len(df) > 0:
         for index, row in df.iterrows():
@@ -610,13 +610,13 @@ if authentication_status:
                     st.write("**Total à payer :**",str(row["montant_final"]) + " €")
             with col2:
                 st.write("**Statut :**", row['statut'])
-                st.write("**Réception douane :**",row.get('Reception_douane', 'En attente'))
+                st.write("**Réception douane :**",row.get('Reception_douane', 'En attente de validation'))
                 if username == "douane":
-                   if row.get('reception_douane') != "Recu":
+                   if row.get('reception_douane') != "validation douane":
                         if st.button("Marquer comme reçu", key=f"reçu_{index}"):
                             try:
                                 result = supabase.table("declaration").update(
-                                    {"Reception_douane": "reçu"}
+                                    {"Reception_douane": "validation douane"}
                                 ).eq("nom_navire", row['nom_navire']).eq("date_entree", row['date_entree']).execute()
                                 st.write(result)
                                 st.rerun()
