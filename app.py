724cdbfcc7ab01db51f85d85f1f60a8e7207374d
marjoryplacide-gif <marjory.placide21@gmail.com>
2026-05-05 17:15:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -81,15 +81,11 @@ def verifier_date_escale(nom_navire, date_entree):
         if df_escales is None:
             return False
         df_escales.columns = df_escales.columns.str.strip()
-        col_date = [c for c in df_escales.columns if "date" in c.lower() and "entr" in c.lower()][0]
-        col_navire = [c for c in df_escales.columns if "navire" in c.lower()][0]
-        st.write("Date brute dans fichier :", df_escales[col_date].iloc[0])
-        st.write("Date cherchee :", date_entree)
-        df_escales["Date d'entree"] = pd.to_datetime(df_escales["Date d'entree"], dayfirst=True, errors='coerce')
+        df_escales[col_date] = pd.to_datetime(df_escales[col_date], format="%d/%m/%Y", errors='coerce')
         date_choisie = pd.to_datetime(date_entree, format="%d/%m/%Y")
         df_filtre = df_escales[
-            (df_escales["Nom du navire"].str.strip() == nom_navire.strip()) &
-            (df_escales["Date d'entree"].dt.date == date_choisie.date())
+            (df_escales[col_navire].str.strip() == nom_navire.strip()) &
+            (df_escales[col_date].dt.date == date_choisie.date())
         ]
         return len(df_filtre) > 0
     except:
